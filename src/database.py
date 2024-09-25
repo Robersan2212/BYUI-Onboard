cfaf437a28cf1849258c18db21ff034a6efe9867
Roberto <san20086@byui.edu>
2024-09-25 09:37:08 -0600

    modified database and connected succesfully.

--- src/database.py
+++ src/database.py
@@ -1,21 +1,8 @@
 import streamlit as st
-from pymongo import MongoClient
-import pandas as pd
+from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from urllib.parse import quote_plus
 
-username = st.secrets['USERNAME']
-db_password = st.secrets['PASSWORD']
-db = st.secrets['DATA_BASE']
-collection = st.secrets['COLLECTION']
-uri_base = st.secrets['MONGODB_URI_BASE']
-
-# Construct the full URI with escaped username and password
-mongodb_uri = uri_base.format(
-    username=quote_plus(username),
-    password=quote_plus(db_password)
-)
-
+mongodb_uri = st.secrets['MONGODB_URI']
 
 # Create a new client and connect to the server
 client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
